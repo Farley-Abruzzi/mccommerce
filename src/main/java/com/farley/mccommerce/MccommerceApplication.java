959a2979d30937fddbdfc0b4463a159d9031ded4
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-06 15:18:03 -0300

    Criando operacao de instanciacao

--- src/main/java/com/farley/mccommerce/MccommerceApplication.java
+++ src/main/java/com/farley/mccommerce/MccommerceApplication.java
@@ -1,13 +1,34 @@
 package com.farley.mccommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MccommerceApplication {
+import com.farley.mccommerce.domain.Categoria;
+import com.farley.mccommerce.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MccommerceApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MccommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }

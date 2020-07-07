5a69658d9c8068dbfa0f919af9c04024f8cd837a
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-07 10:08:11 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/farley/mccommerce/domain/Produto.java
+++ src/main/java/com/farley/mccommerce/domain/Produto.java
@@ -8,29 +8,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	public Produto() {
 	}
-	
-	public Categoria(Integer id, String nome) {
+
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-	
 
 	public Integer getId() {
 		return id;
@@ -40,6 +47,14 @@ public class Categoria implements Serializable {
 		return nome;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
@@ -47,13 +62,13 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -72,7 +87,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -80,6 +95,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+	
 
-		
 }
---
+++ src/main/java/com/farley/mccommerce/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.farley.mccommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farley.mccommerce.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+}

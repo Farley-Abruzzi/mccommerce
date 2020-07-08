d545e0398219ecc8118169365374914f959e81e4
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-08 11:09:25 -0300

    Endpoint /clintes/{id} disponivel

---
+++ src/main/java/com/farley/mccommerce/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.farley.mccommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.farley.mccommerce.domain.Cliente;
+import com.farley.mccommerce.repositories.ClienteRepository;
+import com.farley.mccommerce.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		// tipo do objeto que trouxe essa exceção
+	}
+
+}

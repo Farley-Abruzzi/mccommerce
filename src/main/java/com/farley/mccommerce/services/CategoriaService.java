55c1569cab6f8c6b5abe5fb1db63e2ed96a96ac9
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-09 16:36:55 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/farley/mccommerce/services/CategoriaService.java
+++ src/main/java/com/farley/mccommerce/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.farley.mccommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }

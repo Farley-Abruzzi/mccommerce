daebd4db26da32b098998f5c3507e956ecd0d9e3
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-21 11:46:46 -0300

    Validacao customizada: email não repetido na insercao de Cliente

--- src/main/java/com/farley/mccommerce/repositories/ClienteRepository.java
+++ src/main/java/com/farley/mccommerce/repositories/ClienteRepository.java
@@ -2,11 +2,13 @@ package com.farley.mccommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.farley.mccommerce.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
-
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/farley/mccommerce/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/farley/mccommerce/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.farley.mccommerce.domain.Cliente;
 import com.farley.mccommerce.domain.enums.TipoCliente;
 import com.farley.mccommerce.dto.ClienteNewDTO;
+import com.farley.mccommerce.repositories.ClienteRepository;
 import com.farley.mccommerce.resources.exception.FieldMessage;
 import com.farley.mccommerce.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {

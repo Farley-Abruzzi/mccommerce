37bcdfe5b684f23dde069d944a096eb41d3c37b0
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-21 14:54:08 -0300

    Validacao customizada: email nao repetido na atualizacao do Cliente

--- src/main/java/com/farley/mccommerce/dto/ClienteDTO.java
+++ src/main/java/com/farley/mccommerce/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.farley.mccommerce.domain.Cliente;
+import com.farley.mccommerce.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	

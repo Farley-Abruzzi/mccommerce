3d1fce82caabebc058d3ea900b22e4801e5a471d
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-22 17:07:28 -0300

    Inserindo Pedido

--- src/main/java/com/farley/mccommerce/domain/PagamentoComCartao.java
+++ src/main/java/com/farley/mccommerce/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.farley.mccommerce.domain;
 import javax.persistence.Entity;
 
 import com.farley.mccommerce.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	

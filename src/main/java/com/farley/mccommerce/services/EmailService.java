b96e980b5f650fd374f2215d7c057c816c96a25b
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-07-31 15:18:30 -0300

    Email HTML

--- src/main/java/com/farley/mccommerce/services/EmailService.java
+++ src/main/java/com/farley/mccommerce/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.farley.mccommerce.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.farley.mccommerce.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
